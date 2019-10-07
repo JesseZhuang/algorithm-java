e5c0b371ee54ca63e88d673c96f6c13c2efdf4dc
Jesse Zhuang <zexiz@amazon.com>
2019-10-06 21:54:09 -0700

    remove .gradle/, add guava dependency, add file utils, add unit test AVLTreeST

---
+++ src/main/java/util/FileUtils.java
@@ -0,0 +1,57 @@
+package util;
+
+import com.google.common.io.CharStreams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FileUtils {
+    private static final Logger logger = Logger.getLogger(FileUtils.class.getName());
+
+    private static final String CLASS_PATH_ERROR_MESSAGE = "Failed reading class path file %s.";
+    private static final String FILE_PATH_ERROR_MESSAGE = "Failed reading file %s.";
+
+    public static String readClassPathFileToString(String resourcePath) {
+        try {
+            InputStream in = FileUtils.class.getResourceAsStream(resourcePath);
+            return CharStreams.toString(new InputStreamReader(in, StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "exception ", e);
+            throw new RuntimeException(String.format(CLASS_PATH_ERROR_MESSAGE, resourcePath));
+        }
+    }
+
+    public static Properties loadClassPathProperty(String resourcePath) {
+        try {
+            Properties properties = new Properties();
+            InputStream in = FileUtils.class.getResourceAsStream(resourcePath);
+            properties.load(in);
+            return properties;
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "exception ", e);
+            throw new RuntimeException(String.format(CLASS_PATH_ERROR_MESSAGE, resourcePath));
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static Properties loadFilePathProperty(String resourcePath) {
+        try {
+            Properties properties = new Properties();
+            InputStream in = Files.newInputStream(Paths.get(resourcePath), StandardOpenOption.READ);
+            properties.load(in);
+            return properties;
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "exception ", e);
+            throw new RuntimeException(String.format(FILE_PATH_ERROR_MESSAGE, resourcePath));
+        }
+    }
+
+}
---
+++ src/test/java/tree/AVLTreeTest.java
@@ -0,0 +1,25 @@
+package tree;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import princeton.jsl.AVLTreeST;
+import util.FileUtils;
+
+import java.util.Arrays;
+
+class AVLTreeTest {
+    static AVLTreeST<String, Integer> tbt;
+
+    @BeforeAll
+    static void setup() {
+        tbt = new AVLTreeST<>();
+    }
+
+    @Test
+    void testTinyST() {
+        String[] keys = FileUtils.readClassPathFileToString("/tree/tinyST.txt").split("\\s");
+        System.out.println(Arrays.toString(keys));
+        for (int i = 0; i < keys.length; i++) tbt.put(keys[i], i);
+        for (String s : tbt.keysInOrder()) System.out.println(s + " " + tbt.get(s));
+    }
+}
