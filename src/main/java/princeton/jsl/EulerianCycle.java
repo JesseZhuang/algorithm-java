97b945f5ba0c7731503ad4c010b2d4c086de2c05
Jesse Zhuang <zhzexi@gmail.com>
2020-08-26 16:03:44 -0700

    eulerian cycle jsl

---
+++ src/main/java/princeton/jsl/EulerianCycle.java
@@ -0,0 +1,187 @@
+package princeton.jsl;
+
+
+import java.util.*;
+
+/**
+ * Implement with jsl queue.
+ */
+public class EulerianCycle {
+    private Stack<Integer> cycle = new Stack<Integer>();  // Eulerian cycle; null if no such cycle
+
+    // an undirected edge, with a field to indicate whether the edge has already been used
+    private static class Edge {
+        private final int v;
+        private final int w;
+        private boolean isUsed;
+
+        public Edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+            isUsed = false;
+        }
+
+        // returns the other vertex of the edge
+        public int other(int vertex) {
+            if (vertex == v) return w;
+            else if (vertex == w) return v;
+            else throw new IllegalArgumentException("Illegal endpoint");
+        }
+    }
+
+    /**
+     * Computes an Eulerian cycle in the specified graph, if one exists.
+     *
+     * @param G the graph
+     */
+    public EulerianCycle(Graph G) {
+
+        // must have at least one edge
+        if (G.E() == 0) return;
+
+        // necessary condition: all vertices have even degree
+        // (this test is needed or it might find an Eulerian path instead of cycle)
+        for (int v = 0; v < G.V(); v++)
+            if (G.degree(v) % 2 != 0)
+                return;
+
+        // create local view of adjacency lists, to iterate one vertex at a time
+        // the helper Edge data type is used to avoid exploring both copies of an edge v-w
+        List<Queue<Edge>> adj = new ArrayList<>();
+        for (int v = 0; v < G.V(); v++)
+            adj.add(new ArrayDeque<>());
+
+        for (int v = 0; v < G.V(); v++) {
+            int selfLoops = 0;
+            for (int w : G.adj(v)) {
+                // careful with self loops, G.addEdge(2, 2) will result in 2 parallel self loops. 2:2,2
+                if (v == w) {
+                    if (selfLoops % 2 == 0) {
+                        Edge e = new Edge(v, w);
+                        adj.get(v).add(e);
+                        adj.get(w).add(e);
+                    }
+                    selfLoops++;
+                } else if (v < w) {
+                    Edge e = new Edge(v, w);
+                    adj.get(v).add(e);
+                    adj.get(w).add(e);
+                }
+            }
+        }
+
+        // initialize stack with any non-isolated vertex
+        int s = nonIsolatedVertex(G);
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(s);
+
+        // greedily search through edges in iterative DFS style
+        cycle = new Stack<Integer>();
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            while (!adj.get(v).isEmpty()) {
+                Edge edge = adj.get(v).remove();
+                if (edge.isUsed) continue;
+                edge.isUsed = true;
+                stack.push(v);
+                v = edge.other(v);
+            }
+            // push vertex with no more leaving edges to cycle
+            cycle.push(v);
+        }
+
+        // check if all edges are used
+        if (cycle.size() != G.E() + 1)
+            cycle = null;
+
+        assert certifySolution(G);
+    }
+
+    /**
+     * Returns the sequence of vertices on an Eulerian cycle.
+     *
+     * @return the sequence of vertices on an Eulerian cycle;
+     * {@code null} if no such cycle
+     */
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+
+    /**
+     * Returns true if the graph has an Eulerian cycle.
+     *
+     * @return {@code true} if the graph has an Eulerian cycle;
+     * {@code false} otherwise
+     */
+    public boolean hasEulerianCycle() {
+        return cycle != null;
+    }
+
+    // returns any non-isolated vertex; -1 if no such vertex
+    private static int nonIsolatedVertex(Graph G) {
+        for (int v = 0; v < G.V(); v++)
+            if (G.degree(v) > 0)
+                return v;
+        return -1;
+    }
+
+    /**************************************************************************
+     *
+     *  The code below is solely for testing correctness of the data type.
+     *
+     **************************************************************************/
+
+    // Determines whether a graph has an Eulerian cycle using necessary
+    // and sufficient conditions (without computing the cycle itself):
+    //    - at least one edge
+    //    - degree(v) is even for every vertex v
+    //    - the graph is connected (ignoring isolated vertices)
+    private static boolean satisfiesNecessaryAndSufficientConditions(Graph G) {
+
+        // Condition 0: at least 1 edge
+        if (G.E() == 0) return false;
+
+        // Condition 1: degree(v) is even for every vertex
+        for (int v = 0; v < G.V(); v++)
+            if (G.degree(v) % 2 != 0)
+                return false;
+
+        // Condition 2: graph is connected, ignoring isolated vertices
+        int s = nonIsolatedVertex(G);
+        BreadthFirstPaths bfs = new BreadthFirstPaths(G, s);
+        for (int v = 0; v < G.V(); v++)
+            if (G.degree(v) > 0 && !bfs.hasPathTo(v))
+                return false;
+
+        return true;
+    }
+
+    // check that solution is correct
+    private boolean certifySolution(Graph G) {
+
+        // internal consistency check
+        if (hasEulerianCycle() == (cycle() == null)) return false;
+
+        // hashEulerianCycle() returns correct value
+        if (hasEulerianCycle() != satisfiesNecessaryAndSufficientConditions(G)) return false;
+
+        // nothing else to check if no Eulerian cycle
+        if (cycle == null) return true;
+
+        // check that cycle() uses correct number of edges
+        if (cycle.size() != G.E() + 1) return false;
+
+        // check that cycle() is a cycle of G
+        // TODO
+
+        // check that first and last vertices in cycle() are the same
+        int first = -1, last = -1;
+        for (int v : cycle()) {
+            if (first == -1) first = v;
+            last = v;
+        }
+        if (first != last) return false;
+
+        return true;
+    }
+}
