632645e76e4fc264649b1516635b1523b6279012
Jesse Zhuang <zhzexi@gmail.com>
2022-11-13 21:34:40 -0800

    add unit test for key indexed counting

--- src/test/java/string/KeyIndexedCountingTest.java
+++ src/test/java/string/KeyIndexedCountingTest.java
@@ -1,10 +1,13 @@
 package string;
 
+import junit.converter.IntegerArrayConverter;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import util.IntArrayUtil;
-import junit.converter.IntegerArrayConverter;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
@@ -17,5 +20,27 @@ public class KeyIndexedCountingTest {
         int[] input = IntArrayUtil.unBoxIntegerArray(a);
         KeyIndexedCounting.sort(input, R);
         assertArrayEquals(IntArrayUtil.unBoxIntegerArray(sorted), input);
+    }
+
+    @Test
+    void testSortCharacters() {
+        char[] characters = new char[]{'b', 'a', 'c', 'b'};
+        int[] numbers = new int[characters.length];
+        for (int i = 0; i < characters.length; i++) numbers[i] = convertCharToInt(characters[i]);
+        System.out.println(Arrays.toString(numbers));
+        KeyIndexedCounting.sort(numbers, 128); // assuming lower case letters, use ascii to avoid shifting
+        System.out.println(Arrays.toString(numbers));
+        for (int i = 0; i < characters.length; i++) characters[i] = convertIntToChar(numbers[i]);
+        assertArrayEquals(new char[]{'a', 'b', 'b', 'c'}, characters);
+    }
+
+    private int convertCharToInt(char c) {
+        return c;
+    }
+
+    // https://en.wikibooks.org/wiki/Unicode/Character_reference/0000-0FFF
+    private char convertIntToChar(int i) {
+        if (i > 65536) throw new IllegalArgumentException("integer is too lager, > 2 bytes");
+        return (char) i;
     }
 }
