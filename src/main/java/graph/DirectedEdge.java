7a3adb67c4e788ff8bd49af3534247e8e11bb479
Jesse Zhuang <zhzexi@gmail.com>
2022-11-21 16:19:25 -0800

    add directed edge, weight optional

---
+++ src/main/java/graph/DirectedEdge.java
@@ -0,0 +1,52 @@
+package graph;
+
+import java.util.Optional;
+
+/**
+ * directed edge.
+ */
+public class DirectedEdge {
+    private int v;
+    private int w;
+    private Optional<Double> weight = Optional.empty();
+
+    public DirectedEdge(int v, int w) {
+        this.v = v;
+        this.w = w;
+    }
+
+    public DirectedEdge(int v, int w, double weight) {
+        this(v, w);
+        this.weight = Optional.of(weight);
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public int other(int vertex) {
+        if (vertex == v) return w;
+        else if (vertex == w) return v;
+        else throw new IllegalArgumentException("edge does not contain vertex: " + vertex);
+    }
+
+    @Override
+    public String toString() {
+        return "DirectedEdge{" +
+                "v=" + v +
+                ", w=" + w +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        DirectedEdge edge = new DirectedEdge(1, 3);
+        System.out.println(edge);
+        edge = new DirectedEdge(1, 5, 1.23);
+        System.out.println(edge);
+    }
+}
