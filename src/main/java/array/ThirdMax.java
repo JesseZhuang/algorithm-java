cfa0f833992185548c0b2aeb81ed54c3047c6d61
Jesse Zhuang <zhzexi@gmail.com>
2022-11-25 22:32:39 -0800

    add more to third max

--- src/main/java/array/ThirdMax.java
+++ src/main/java/array/ThirdMax.java
@@ -2,15 +2,25 @@ package array;
 
 import util.IntArrayUtil;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
+ * <p>
+ * LeetCode 414 Easy. Tags: Array, Sorting.
  * Find third distinct maximum in the array.
  * <p>
  * kth distinct Maximum
  * <p>
- * O(N) for quick select, need to handle duplicates
+ * <ul>
+ *     <ui>sort, O(NLgN) time, O(1) space</ui>
+ *     <ui>min heap, O(N) time, O(1) space</ui>
+ *     <ui>tree set, O(N) time, O(1) space</ui>
+ *     <ui>quick select, set to dedupe, O(N) time, O(N) space</ui>
+ * </ul>
  */
 public class ThirdMax {
     /**
@@ -39,6 +49,89 @@ public class ThirdMax {
         }
     }
 
+    public int thirdMaxSort(int[] nums) {
+        Arrays.sort(nums);
+        // Reverse array to make it non-increasing.
+        for (int index = 0; index < nums.length / 2; ++index) {
+            int temp = nums[index];
+            nums[index] = nums[nums.length - 1 - index];
+            nums[nums.length - 1 - index] = temp;
+        }
+        int elemCounted = 1;
+        int prevElem = nums[0];
+        for (int index = 1; index < nums.length; ++index) {
+            // Current element is different from previous.
+            if (nums[index] != prevElem) {
+                elemCounted += 1;
+                prevElem = nums[index];
+            }
+            // If we have counted 3 numbers then return current number.
+            if (elemCounted == 3) {
+                return nums[index];
+            }
+        }
+        // We never counted 3 distinct numbers, return largest number.
+        return nums[0];
+    }
+
+    public int thirdMaxMinHeap(int[] nums) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        Set<Integer> taken = new HashSet<>();
+        for (int index = 0; index < nums.length; ++index) {
+            // If current number was already taken, skip it.
+            if (taken.contains(nums[index])) {
+                continue;
+            }
+            // If min heap already has three numbers in it.
+            // Pop the smallest if current number is bigger than it.
+            if (minHeap.size() == 3) {
+                if (minHeap.peek() < nums[index]) {
+                    taken.remove(minHeap.poll());
+                    minHeap.add(nums[index]);
+                    taken.add(nums[index]);
+                }
+            }
+            // If min heap does not have three number we can push it.
+            else {
+                minHeap.add(nums[index]);
+                taken.add(nums[index]);
+            }
+        }
+        // 'nums' has only one distinct element it will be the maximum.
+        if (minHeap.size() == 1) return minHeap.peek();
+            // 'nums' has two distinct elements.
+        else if (minHeap.size() == 2) {
+            int firstNum = minHeap.poll();
+            return Math.max(firstNum, minHeap.peek());
+        }
+        return minHeap.peek();
+    }
+
+    public int thirdMaxTreeSet(int[] nums) {
+        TreeSet<Integer> sortedNums = new TreeSet<>();
+        for (int num : nums) {
+            if (sortedNums.contains(num)) continue;
+            if (sortedNums.size() == 3) {
+                // And the smallest element is smaller than current element.
+                if (sortedNums.first() < num) {
+                    // Then remove the smallest element and push the current element.
+                    sortedNums.pollFirst();
+                    sortedNums.add(num);
+                }
+            }
+            // Otherwise push the current element of nums array.
+            else {
+                sortedNums.add(num);
+            }
+        }
+        // If sorted set has three elements return the smallest among those 3.
+        if (sortedNums.size() == 3) {
+            return sortedNums.first();
+        }
+        // Otherwise return the biggest element of nums array.
+        return sortedNums.last();
+    }
+
     /**
      * O(N) time, O(N) space. Uses quick select.
      */
@@ -53,7 +146,6 @@ public class ThirdMax {
         if (set.size() < 3) return max;
         else return QuickSelect.kthLargest(candyNumbers, 0, candyNumbers.length - 1, 3);
     }
-
 
     public static void main(String[] args) {
         int[][] candies = new int[][]{
