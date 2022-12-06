ddce1df7c8b4d26b946008ded60e56ece9586fb1
Jesse Zhuang <zhzexi@gmail.com>
2022-12-06 14:38:22 -0800

    leetcode 153 find min in sorted rotated array

---
+++ src/main/java/array/FindMinRotatedSortedArray.java
@@ -0,0 +1,57 @@
+package array;
+
+/**
+ * LeetCode 153, medium, tags: array, binary search.
+ * <p>
+ * Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+ * <p>
+ * [4,5,6,7,0,1,2] if it was rotated 4 times.
+ * [0,1,2,4,5,6,7] if it was rotated 7 times.
+ * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+ * <p>
+ * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+ * <p>
+ * You must write an algorithm that runs in O(log n) time.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,4,5,1,2]
+ * Output: 1
+ * Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+ * Example 2:
+ * <p>
+ * Input: nums = [4,5,6,7,0,1,2]
+ * Output: 0
+ * Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+ * Example 3:
+ * <p>
+ * Input: nums = [11,13,15,17]
+ * Output: 11
+ * Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+ * <p>
+ * <p>
+ * Constraints:
+ * <pre>
+ * n == nums.length
+ * 1 <= n <= 5000
+ * -5000 <= nums[i] <= 5000
+ * All the integers of nums are unique.
+ * nums is sorted and rotated between 1 and n times.
+ * </pre>
+ */
+public class FindMinRotatedSortedArray {
+    /**
+     * O(lgN) time, O(1) space. 0ms, 42.8 Mb.
+     */
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) left = mid + 1; // min must be on the right half
+            else right = mid; // min must be on left half including mid
+        }
+        return nums[left];
+    }
+}
