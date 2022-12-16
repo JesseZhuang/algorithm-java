48ac44f255dda41c3aa773d899a3b4d5d9c4a46c
Jesse Zhuang <zhzexi@gmail.com>
2022-12-15 21:00:28 -0800

    add ood canvas question

---
+++ src/main/java/ood/Canvas.java
@@ -0,0 +1,21 @@
+package ood;
+
+/**
+ * basic app drawing canvas. microsoft paint or adobe photoshop simplified.
+ * Operations:
+ * <ul>
+ *     <ui>add shapes to canvas</ui>
+ *     <ui>move shapes around the canvas</ui>
+ * </ul>
+ * Clarifications:
+ * <ul>
+ *     <ui>0 means a white space</ui>
+ *     <ui>support multiple shapes, can overlap, last shape drawn displayed</ui>
+ *     <ui>part 1, create MxN blank canvas, display 0</ui>
+ *     <ui>part 2, draw rectangle of size L*W and place at some coordinate (i,j)</ui>
+ *     <ui>part 3, move the shape to any new location within canvas, note previously covered shape may show</ui>
+ * </ul>
+ *
+ */
+public class Canvas {
+}
