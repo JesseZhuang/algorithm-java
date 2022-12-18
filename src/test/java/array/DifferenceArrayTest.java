975c61383eca809564533f5505c5fd0e42548672
Jesse Zhuang <zhzexi@gmail.com>
2022-12-18 02:56:52 -0800

    fix greg array, second fold diff array, delta implicit 1

--- src/test/java/array/DifferenceArrayTest.java
+++ src/test/java/array/DifferenceArrayTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -31,17 +30,37 @@ class DifferenceArrayTest {
         assertArrayEquals(new int[]{20, 35, 70, 50, -7, -4}, tbt.getOriginal());
     }
 
-    // https://codeforces.com/contest/295/problem/A
+    /**
+     * https://codeforces.com/contest/295/problem/A
+     *
+     * @param original,        size n, e.g., [1,2,3]
+     * @param operations,      size m, index are 1 based, [left, right, delta], inclusive
+     * @param whichOpsToApply, size k, ops range: O(m), index are 1 based, [left, right], inclusive
+     * @param expected,        size n
+     */
     @ParameterizedTest
     @MethodSource({"paramProvider"})
     void testGregArray(int[] original, int[][] operations, int[][] whichOpsToApply, int[] expected) {
-        DifferenceArray diff = new DifferenceArray(original);
-        System.out.println(Arrays.toString(original));
-        for (int[] whichOps : whichOpsToApply) {
-            for (int i = whichOps[0]; i <= whichOps[1]; i++)
+        DifferenceArray diff = new DifferenceArray(original); // O(n)
+        // O(k*m+2n)
+        for (int[] whichOps : whichOpsToApply) { // O(k)
+            for (int i = whichOps[0]; i <= whichOps[1]; i++) // O(m)
                 diff.update(operations[i - 1][0] - 1, operations[i - 1][1] - 1, operations[i - 1][2]);
         }
-        assertArrayEquals(expected, diff.getOriginal());
+        assertArrayEquals(expected, diff.getOriginal()); // O(n)
+        // O(k+2m+2n)
+        diff = new DifferenceArray(original); // O(n)
+        int m = operations.length;
+        DifferenceArray diff2 = new DifferenceArray(new int[m]); // O(m)
+        for (int[] whichOps : whichOpsToApply) // O(k)
+            diff2.update(whichOps[0] - 1, whichOps[1] - 1, 1);
+        int[] opsCount = diff2.getOriginal(); // O(m)
+        for (int i = 0; i < m; i++) { // O(m)
+            if (opsCount[i] > 0)
+                diff.update(operations[i][0] - 1,
+                        operations[i][1] - 1, operations[i][2] * opsCount[i]);
+        }
+        assertArrayEquals(expected, diff.getOriginal()); // O(n)
     }
 
     static Stream<Arguments> paramProvider() {
