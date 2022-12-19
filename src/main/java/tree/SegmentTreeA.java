4db7a9b53369ca93cbb7364b274439f88226b989
Jesse Zhuang <zhzexi@gmail.com>
2022-12-19 12:47:42 -0800

    continue implementation segment tree

--- src/main/java/tree/SegmentTreeA.java
+++ src/main/java/tree/SegmentTreeA.java
@@ -12,16 +12,19 @@ public class SegmentTreeA {
     }
 
     private Node[] heap;
+    private int n;
 
     // initialization O(n) time, O(n) space.
     public SegmentTreeA(int[] nums) {
-        // if 0 based heap, size is 2 * [2^ceil(Log2(n))] - 1. ceil(Log2(n): height of tree
+        n = nums.length;
+        // 0 based heap, size is 2 * [2^ceil(Log2(n))] - 1. ceil(Log2(n): height of tree
+        // 1 based heap, size is 2 * [2^floor(Log2(n))] + 1, O(n) space
         heap = new Node[2 * (int) (Math.pow(2.0, Math.ceil(Math.log(nums.length) / Math.log(2.0)))) - 1];
-        build(nums, 1, 0, nums.length - 1);
+        build(nums, 0, 0, n - 1);
     }
 
     /**
-     * recursive helper to build the segment tree.
+     * recursive helper to build the segment tree. O(n) time build tree.
      *
      * @param nums  original array
      * @param ci    index of current node in segment tree
@@ -33,8 +36,11 @@ public class SegmentTreeA {
         if (left == right) heap[ci].sum = heap[ci].min = heap[ci].max = nums[left];
         else {
             int mid = mid(left, right);
-            build(nums, 2 * ci, left, mid);
-            build(nums, 2 * ci + 1, mid + 1, right);
+            build(nums, 2 * ci + 1, left, mid);
+            build(nums, 2 * ci + 2, mid + 1, right);
+            heap[ci].sum = heap[2 * ci + 1].sum + heap[2 * ci + 2].sum;
+            heap[ci].min = Math.min(heap[2 * ci + 1].min, heap[2 * ci + 2].min);
+            heap[ci].max = Math.min(heap[2 * ci + 1].max, heap[2 * ci + 2].max);
         }
     }
 
@@ -43,7 +49,14 @@ public class SegmentTreeA {
     }
 
     public int rsq(int left, int right) {
-        return 0;
+        return rsq(0, left, right, 0, n - 1);
+    }
+
+    private int rsq(int ci, int left, int right, int sLeft, int sRight) {
+        if (left <= sLeft && right >= sRight) return heap[ci].sum; // query range contains search range
+        if (left > sRight || right < sLeft) return 0; // query range is outside
+        int mid = mid(left, right);
+        return rsq(2 * ci + 1, left, right, sLeft, mid) + rsq(2 * ci + 2, left, right, mid + 1, sRight);
     }
 
     public int rMinQ(int left, int right) {
