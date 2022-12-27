e434207124b3ec9ad88ef90164a63e73573a700a
Jesse Zhuang <zhzexi@gmail.com>
2022-12-26 21:59:15 -0800

    leetcode 55 add question

---
+++ src/main/java/dp/JumpGame.java
@@ -0,0 +1,31 @@
+package dp;
+
+/**
+ * LeetCode 55, medium, tags: array, dynamic programming, greedy.
+ * You are given an integer array nums. You are initially positioned at the array's first index, and each element
+ * in the array represents your maximum jump length at that position.
+ * <p>
+ * Return true if you can reach the last index, or false otherwise.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ * Example 2:
+ * <p>
+ * Input: nums = [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it
+ * impossible to reach the last index.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * 0 <= nums[i] <= 105
+ */
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        return false;
+    }
+}
