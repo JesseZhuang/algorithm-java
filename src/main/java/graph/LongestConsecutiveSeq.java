10314d007076b3f13a5e3c779b0654eb2278796e
Jesse Zhuang <zhzexi@gmail.com>
2023-01-03 12:23:47 -0800

    leetcode 128 q

---
+++ src/main/java/graph/LongestConsecutiveSeq.java
@@ -0,0 +1,28 @@
+package graph;
+
+/**
+ * LeetCode 128, medium, tags: array, hash table, union find.
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * <p>
+ * You must write an algorithm that runs in O(n) time.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * Example 2:
+ * <p>
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ */
+public class LongestConsecutiveSeq {
+    public int longestConsecutive(int[] nums) {
+        return 0;
+    }
+}
