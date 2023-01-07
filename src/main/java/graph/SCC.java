0f03f7614362395e46994ab838dad2f2273b889f
Jesse Zhuang <zhzexi@gmail.com>
2023-01-06 16:35:36 -0800

    debug study gabow scc

--- src/main/java/graph/SCC.java
+++ src/main/java/graph/SCC.java
@@ -9,28 +9,39 @@ import java.util.List;
 
 public class SCC {
 
+    // dfs reverse graph, with the reverse post order, dfs again and count
     public static List<List<Integer>> components(KosarajuSharirSCC scc, int nV) {
         int[] id = new int[nV];
-        for(int v = 0; v < nV; v++) id[v] = scc.id(v);
+        for (int v = 0; v < nV; v++) id[v] = scc.id(v);
         return components(id, nV, scc.count());
     }
 
+    // two stacks, id[] to identify cycle
     public static List<List<Integer>> components(GabowSCC scc, int nV) {
         int[] id = new int[nV];
-        for(int v = 0; v < nV; v++) id[v] = scc.id(v);
+        for (int v = 0; v < nV; v++) id[v] = scc.id(v);
         return components(id, nV, scc.count());
     }
 
+    // low[] to identify cycle, one stack
     public static List<List<Integer>> components(TarjanSCC scc, int nV) {
         int[] id = new int[nV];
-        for(int v = 0; v < nV; v++) id[v] = scc.id(v);
+        for (int v = 0; v < nV; v++) id[v] = scc.id(v);
         return components(id, nV, scc.count());
     }
 
+    /**
+     * gather the strongly connected components.
+     *
+     * @param id    the id array from the scc classes.
+     * @param nV    number of vertexes
+     * @param count number of scc
+     * @return the components.
+     */
     private static List<List<Integer>> components(int[] id, int nV, int count) {
         List<List<Integer>> result = new ArrayList<>(count);
         for (int i = 0; i < count; i++) result.add(i, new ArrayList<>());
-        for(int v = 0; v < nV; v++) result.get(id[v]).add(v);
+        for (int v = 0; v < nV; v++) result.get(id[v]).add(v);
         return result;
     }
 
--- src/test/java/graph/SCCTest.java
+++ src/test/java/graph/SCCTest.java
@@ -1,8 +1,10 @@
 package graph;
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.GabowSCC;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.KosarajuSharirSCC;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -18,6 +20,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * instead of two. However, Kosaraju's algorithm is simple in implementation.
  */
 public class SCCTest {
+
+    Digraph digraph1;
+
+    @BeforeEach
+    void setup() {
+        digraph1 = new Digraph(6);
+        digraph1.addEdge(0, 1); // cycle 0,1,2,0
+        digraph1.addEdge(1, 2);
+        digraph1.addEdge(2, 0);
+        digraph1.addEdge(3, 4); // 3->4, not strongly connected
+    }
+
     @Test
     void testDAG() {
         KosarajuSharirSCC scc = new KosarajuSharirSCC(new Digraph(new In("/graph/tinyDG.txt")));
@@ -28,5 +42,16 @@ public class SCCTest {
         components.add(Arrays.asList(6, 8));
         components.add(Arrays.asList(7));
         assertEquals(components, SCC.components(scc, 13));
+    }
+
+    @Test
+    void testGabowSCC() { // add debug point in GabowSCC to see how it works
+        GabowSCC scc = new GabowSCC(digraph1);
+        List<List<Integer>> components = new ArrayList<>();
+        components.add(Arrays.asList(0, 1, 2));
+        components.add(Arrays.asList(4));
+        components.add(Arrays.asList(3));
+        components.add(Arrays.asList(5));
+        assertEquals(components, SCC.components(scc, 6));
     }
 }
