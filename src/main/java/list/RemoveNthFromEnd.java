c82c1db01ae27f130fdd782416249676f2299d7d
Jesse Zhuang <zhzexi@gmail.com>
2023-01-11 14:24:39 -0800

    leetcode 19

---
+++ src/main/java/list/RemoveNthFromEnd.java
@@ -0,0 +1,36 @@
+package list;
+
+import struct.ListNode;
+
+/**
+ * LeetCode 19, medium, tags: linked list, two pointers.
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ * Example 2:
+ * <p>
+ * Input: head = [1], n = 1
+ * Output: []
+ * Example 3:
+ * <p>
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ * <p>
+ * Follow up: Could you do this in one pass?
+ * Hint1: Maintain two pointers and update one with a delay of n steps.
+ */
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+}
