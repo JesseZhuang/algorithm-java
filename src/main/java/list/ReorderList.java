4a49b8fd3b6c2fce71fa02ad5b931063a7b70c59
Jesse Zhuang <zhzexi@gmail.com>
2023-01-13 11:44:18 -0800

    add normal merge

--- src/main/java/list/ReorderList.java
+++ src/main/java/list/ReorderList.java
@@ -50,6 +50,12 @@ public class ReorderList {
             head1.next = head2;
             head1 = head2;
             head2 = temp;
+
+//            ListNode temp1 = head1.next, temp2 = head2.next;
+//            head1.next = head2;
+//            head2.next = temp1;
+//            head1 = temp1;
+//            head2 = temp2;
         }
     }
 }
