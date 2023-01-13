b5f420e07e9c199bd0e3f73b18f14ff9ee754388
Jesse Zhuang <zhzexi@gmail.com>
2023-01-12 17:44:05 -0800

    leetcode 143

---
+++ src/main/java/list/ReorderList.java
@@ -0,0 +1,55 @@
+package list;
+
+import struct.ListNode;
+
+/**
+ * LeetCode 143, medium, tags: linked list, two pointers, stack, recursion.
+ * You are given the head of a singly linked-list. The list can be represented as:
+ * <p>
+ * L0 → L1 → … → Ln - 1 → Ln
+ * Reorder the list to be on the following form:
+ * <p>
+ * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+ * You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+ * <p>
+ * Example 1: https://assets.leetcode.com/uploads/2021/03/04/reorder1linked-list.jpg
+ * <p>
+ * Input: head = [1,2,3,4]
+ * Output: [1,4,2,3]
+ * Example 2: https://assets.leetcode.com/uploads/2021/03/09/reorder2-linked-list.jpg
+ * <p>
+ * Input: head = [1,2,3,4,5]
+ * Output: [1,5,2,4,3]
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the list is in the range [1, 5 * 10^4].
+ * 1 <= Node.val <= 1000
+ */
+public class ReorderList {
+    // 1ms, 45.3 MB. O(N) time, O(1) space.
+    public void reorderList(ListNode head) {
+        // if (head == null) return;
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        } // at the end slow will be before the section that needs to be reversed
+        ListNode headToReverse = slow.next;
+        ListNode head2 = null;
+        while (headToReverse != null) { // reverse the second half, 12345 -> 123;54
+            ListNode temp = headToReverse.next;
+            headToReverse.next = head2;
+            head2 = headToReverse;
+            headToReverse = temp;
+        }
+        slow.next = null;
+        ListNode head1 = head;
+        while (head2 != null) {// zig zag funny merge
+            ListNode temp = head1.next;
+            head1.next = head2;
+            head1 = head2;
+            head2 = temp;
+        }
+    }
+}
