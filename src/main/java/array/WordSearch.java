25e96ac441c981377358dc466a087d57dbc3e481
Jesse Zhuang <zhzexi@gmail.com>
2023-01-14 20:19:44 -0800

    lc 79 question

---
+++ src/main/java/array/WordSearch.java
@@ -0,0 +1,36 @@
+package array;
+
+/**
+ * LeetCode 79, medium, tags: array, matrix, backtracking. LintCode 123.
+ * Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+ * <p>
+ * The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally
+ * or vertically neighboring. The same letter cell may not be used more than once.
+ * <p>
+ * Example 1: https://assets.leetcode.com/uploads/2020/11/04/word2.jpg
+ * <p>
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+ * Output: true
+ * Example 2: https://assets.leetcode.com/uploads/2020/11/04/word-1.jpg
+ * <p>
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+ * Output: true
+ * Example 3: https://assets.leetcode.com/uploads/2020/10/15/word3.jpg
+ * <p>
+ * Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+ * Output: false
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * m == board.length
+ * n = board[i].length
+ * 1 <= m, n <= 6
+ * 1 <= word.length <= 15
+ * board and word consists of only lowercase and uppercase English letters.
+ */
+public class WordSearch {
+    public boolean exist(char[][] board, String word) {
+        return false;
+    }
+}
