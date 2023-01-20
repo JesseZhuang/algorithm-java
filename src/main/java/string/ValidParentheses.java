9c514ae950e8911b0d5fc6306ac536800b0a7c92
Jesse Zhuang <zhzexi@gmail.com>
2023-01-19 16:46:59 -0800

    lc 20

--- src/main/java/string/ValidParentheses.java
+++ src/main/java/string/ValidParentheses.java
@@ -1,5 +1,7 @@
 package string;
 
+import java.util.Stack;
+
 /**
  * LeetCode 20, easy, tags: string, stack.
  * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the
@@ -23,9 +25,22 @@ package string;
  * <p>
  * Input: s = "(]"
  * Output: false
+ * Hints:
+ * Use a stack of characters.
+ * When you encounter an opening bracket, push it to the top of the stack.
+ * When you encounter a closing bracket, check if the top of the stack was the opening for it.
+ * If yes, pop it from the stack. Otherwise, return false.
  */
 public class ValidParentheses {
+    // 1ms, 40.5Mb. O(n) time and space.
     public boolean isValid(String s) {
-        return false;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') stack.push(')');
+            else if (s.charAt(i) == '[') stack.push(']');
+            else if (s.charAt(i) == '{') stack.push('}');
+            else if (stack.isEmpty() || s.charAt(i) != stack.pop()) return false;
+        }
+        return stack.isEmpty(); // still need to check emptiness
     }
 }
