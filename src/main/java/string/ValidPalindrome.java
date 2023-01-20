9cf6d7b4d6c2c9b44987d24ee21ed87d4e70437d
Jesse Zhuang <zhzexi@gmail.com>
2023-01-19 17:53:49 -0800

    lc 125 2pointer

--- src/main/java/string/ValidPalindrome.java
+++ src/main/java/string/ValidPalindrome.java
@@ -1,10 +1,50 @@
 package string;
 
+/**
+ * LeetCode 125, easy, tags: two pointers, string.
+ * A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing
+ * all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include
+ * letters and numbers.
+ * <p>
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * Example 2:
+ * <p>
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ * Example 3:
+ * <p>
+ * Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric characters.
+ * Since an empty string reads the same forward and backward, it is a palindrome.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 2 * 105
+ * s consists only of printable ASCII characters.
+ */
 public class ValidPalindrome {
     // O(n) time and space. 659ms, 43.3Mb.
     public boolean isPalindromeR(String s) {
         String actual = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
         String rev = new StringBuffer(actual).reverse().toString();
         return actual.equals(rev);
+    }
+
+    // 3ms 41.9 Mb. two pointer, O(n) time, O(1) space.
+    public boolean isPalindrome2P(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (!Character.isLetterOrDigit(s.charAt(i)) && i < j) i++;
+            while (!Character.isLetterOrDigit(s.charAt(j)) && i < j) j--;
+            if (Character.toUpperCase(s.charAt(i)) != Character.toUpperCase(s.charAt(j))) return false;
+        }
+        return true;
     }
 }
