a9a8ed6bd89ddc3fd74d4328e061d3a355a8681c
Jesse Zhuang <zhzexi@gmail.com>
2023-01-19 16:32:09 -0800

    lc 20 question

---
+++ src/main/java/string/ValidParentheses.java
@@ -0,0 +1,31 @@
+package string;
+
+/**
+ * LeetCode 20, easy, tags: string, stack.
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the
+ * input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "()"
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: s = "()[]{}"
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: s = "(]"
+ * Output: false
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        return false;
+    }
+}
