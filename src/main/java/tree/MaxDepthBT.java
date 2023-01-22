a8adb8bc3d7a15e4671a87dc2fffe2eefb4b844e
Jesse Zhuang <zhzexi@gmail.com>
2023-01-21 22:33:26 -0800

    lc 104 refactor

--- src/main/java/tree/MaxDepthBT.java
+++ src/main/java/tree/MaxDepthBT.java
@@ -6,13 +6,26 @@ import java.util.HashMap;
 import java.util.Stack;
 
 /**
- * LeetCode 104. Easy.
+ * LeetCode 104. Easy. tags: tree, dfs, bfs, binary tree.
  * <p>
  * Given a binary tree, find its maximum depth.
  * <p>
  * The maximum depth is the number of nodes along the longest path from the root
  * node down to the farthest leaf node.
  * <p>
+ * Example 1: https://assets.leetcode.com/uploads/2020/11/26/tmp-tree.jpg
+ * <p>
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: root = [1,null,2]
+ * Output: 2
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the tree is in the range [0, 104].
+ * -100 <= Node.val <= 100
  * <b>Summary</b>
  * <ul>
  * <li>recursive, max of left and right, O(height) time, O(height) stack space.</li>
@@ -35,6 +48,7 @@ public class MaxDepthBT {
      * iterative version, probably not possible without using a stack. recursive
      * version is using the stack implicitly. can use Apache commons LinkedMap
      * to avoid using a stack (fundamentally still a stack).
+     * 3ms, 42.1 Mb.
      */
     public int maxDepthIter(TreeNode root) {
         if (root == null)
@@ -44,7 +58,6 @@ public class MaxDepthBT {
         st.push(root);
         HashMap<TreeNode, Integer> height = new HashMap<>();
         height.put(root, curLevel);
-
         TreeNode cur = root.left;
         while (!st.isEmpty() || cur != null) {
             if (cur != null) {
@@ -59,21 +72,6 @@ public class MaxDepthBT {
             }
         }
         return maxLevel;
-    }
-
-    // same as alg 1, no need to check left right null
-    @Deprecated
-    public int maxDepth3(TreeNode root) {
-        int ans = 0;
-        if (root != null) {
-            int left = 0, right = 0;
-            if (root.left != null)
-                left = 1 + maxDepthRecursive(root.left);
-            if (root.right != null)
-                right = 1 + maxDepthRecursive(root.right);
-            ans = Math.max(left, right);
-        }
-        return ans;
     }
 
 }
