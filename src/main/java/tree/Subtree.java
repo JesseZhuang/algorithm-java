abea6a7eca0a040c579e487c76175e61ab6a522f
Jesse Zhuang <zhzexi@gmail.com>
2023-01-26 21:34:48 -0800

    refactor lc572

--- src/main/java/tree/Subtree.java
+++ src/main/java/tree/Subtree.java
@@ -98,50 +98,30 @@ public class Subtree {
         return restart;
     }
 
-    // CONSTANTS
-    final int MOD_1 = 1000000007;
+    final int MOD_1 = 1000000007; //BigInteger.probablePrime();
     final int MOD_2 = 2147483647;
 
-    // Hashing a Node
     long[] hashSubtreeAtNode(TreeNode node, boolean needToAdd) {
-
-        if (node == null)
-            return new long[]{3, 7};
-
+        if (node == null) return new long[]{3, 7};
         long[] left = hashSubtreeAtNode(node.left, needToAdd);
         long[] right = hashSubtreeAtNode(node.right, needToAdd);
-
-        long left1 = (left[0] << 5) % MOD_1;
+        long left1 = (left[0] << 5) % MOD_1; // avoid overflow
         long right1 = (right[0] << 1) % MOD_1;
         long left2 = (left[1] << 7) % MOD_2;
         long right2 = (right[1] << 1) % MOD_2;
-
-        long[] hashpair = {(left1 + right1 + node.val) % MOD_1,
+        long[] hashPair = {(left1 + right1 + node.val) % MOD_1,
                 (left2 + right2 + node.val) % MOD_2};
-
-        if (needToAdd)
-            memo.add(hashpair);
-
-        return hashpair;
+        if (needToAdd) memo.add(hashPair);
+        return hashPair;
     }
 
-    // Vector to store hashed value of each node.
     List<long[]> memo = new ArrayList<>();
 
+    // 7ms, 49.4 Mb. hashing. can add check collision with identical() for O(m)
     public boolean isSubtreeHash(TreeNode root, TreeNode subRoot) {
-
-        // Calling and adding hash to List
-        hashSubtreeAtNode(root, true);
-
-        // Storing hashed value of subRoot for comparison
+        hashSubtreeAtNode(root, true); // hashing and memorize hash of all nodes in tree
         long[] s = hashSubtreeAtNode(subRoot, false);
-
-        // Check if hash of subRoot is present in memo
-        for (long[] m : memo) {
-            if (m[0] == s[0] && m[1] == s[1])
-                return true;
-        }
-
+        for (long[] m : memo) if (m[0] == s[0] && m[1] == s[1]) return true;
         return false;
     }
 
