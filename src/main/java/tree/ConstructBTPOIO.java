3bf28c415d78baaf5e8e5c17f1e32aee0367ad60
Jesse Zhuang <zhzexi@gmail.com>
2023-01-28 13:32:21 -0800

    lc 105 question

---
+++ src/main/java/tree/ConstructBTPOIO.java
@@ -0,0 +1,35 @@
+package tree;
+
+import struct.TreeNode;
+
+/**
+ * LeetCode 105, medium, tags: array, hash table, divide and conquer, tree, binary tree.
+ * <p>
+ * Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder
+ * is the inorder traversal of the same tree, construct and return the binary tree.
+ * <p>
+ * Example 1: https://assets.leetcode.com/uploads/2021/02/19/tree.jpg
+ * <p>
+ * Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * Output: [3,9,20,null,null,15,7]
+ * Example 2:
+ * <p>
+ * Input: preorder = [-1], inorder = [-1]
+ * Output: [-1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder and inorder consist of unique values.
+ * Each value of inorder also appears in preorder.
+ * preorder is guaranteed to be the preorder traversal of the tree.
+ * inorder is guaranteed to be the inorder traversal of the tree.
+ */
+public class ConstructBTPOIO {
+    public TreeNode buildTree(int[] preorder, int inorder) {
+        return null;
+    }
+}
