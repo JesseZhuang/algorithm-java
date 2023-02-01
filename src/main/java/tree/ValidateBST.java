343c94c474f3e99fa1c4ee27aae9411ce24c20a3
Jesse Zhuang <zhzexi@gmail.com>
2023-01-31 16:49:43 -0800

    lc 98

--- src/main/java/tree/ValidateBST.java
+++ src/main/java/tree/ValidateBST.java
@@ -2,6 +2,12 @@ package tree;
 
 import struct.TreeNode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * LeetCode 98, medium, tags: tree, binary tree, dfs, bfs.
  * <p>
@@ -33,7 +39,75 @@ import struct.TreeNode;
  * -231 <= Node.val <= 231 - 1
  */
 public class ValidateBST {
-    public boolean isValidBST(TreeNode root) {
-        return false;
+    // O(n) time and space. build in order list. 1ms, 41.5 Mb.
+    public boolean isValidBSTR2(TreeNode root) {
+        List<Integer> inOrder = new ArrayList<>();
+        buildInOrder(root, inOrder);
+        for (int i = 0; i < inOrder.size() - 1; i++)
+            if (inOrder.get(i) >= inOrder.get(i + 1)) return false;
+        return true;
     }
+
+    private void buildInOrder(TreeNode n, List<Integer> inOrder) {
+        if (n == null) return;
+        if (n.left != null) buildInOrder(n.left, inOrder);
+        inOrder.add(n.val);
+        if (n.right != null) buildInOrder(n.right, inOrder);
+    }
+
+    // O(n) time, O(height, worst case n) recursion space, 0ms, 41.6MB.
+    public boolean isValidBSTR1(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode n, Integer low, Integer high) {
+        if (n == null) return true;
+        if ((low != null && n.val <= low) || (high != null && n.val >= high)) return false;
+        return validate(n.left, low, n.val) && validate(n.right, n.val, high);
+    }
+
+    Deque<TreeNode> nodes = new LinkedList<>();
+    Deque<Integer> lows = new LinkedList<>();
+    Deque<Integer> highs = new LinkedList<>();
+
+    // 5ms, 42.7MB.
+    public boolean isValidBSTI2(TreeNode root) {
+        Integer low = null, high = null;
+        update(root, low, high);
+        while (!nodes.isEmpty()) {
+            TreeNode n = nodes.remove();
+            low = lows.remove();
+            high = highs.remove();
+            if (n == null) continue;
+            if (low != null && n.val <= low) return false;
+            if (high != null && n.val >= high) return false;
+            update(n.left, low, n.val);
+            update(n.right, n.val, high);
+        }
+        return true;
+    }
+
+    private void update(TreeNode n, Integer low, Integer high) {
+        nodes.add(n);
+        lows.add(low);
+        highs.add(high);
+    }
+
+    // 2ms, 41.8 Mb.
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Integer prev = null;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prev != null && root.val <= prev) return false;
+            prev = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
 }
