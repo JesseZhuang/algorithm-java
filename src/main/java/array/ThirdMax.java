d8f6def59d3da94e8817d11f710240b2c7d9c87e
Jesse Zhuang <zhzexi@gmail.com>
2023-01-31 19:56:59 -0800

    lc 414 add description and constraints

--- src/main/java/array/ThirdMax.java
+++ src/main/java/array/ThirdMax.java
@@ -10,6 +10,38 @@ import java.util.TreeSet;
 
 /**
  * LeetCode 414 Easy. Tags: Array, Sorting.
+ * Given an integer array nums, return the third distinct maximum number in this array. If the third maximum
+ * does not exist, return the maximum number.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,2,1]
+ * Output: 1
+ * Explanation:
+ * The first distinct maximum is 3.
+ * The second distinct maximum is 2.
+ * The third distinct maximum is 1.
+ * Example 2:
+ * <p>
+ * Input: nums = [1,2]
+ * Output: 2
+ * Explanation:
+ * The first distinct maximum is 2.
+ * The second distinct maximum is 1.
+ * The third distinct maximum does not exist, so the maximum (2) is returned instead.
+ * Example 3:
+ * <p>
+ * Input: nums = [2,2,3,1]
+ * Output: 1
+ * Explanation:
+ * The first distinct maximum is 3.
+ * The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+ * The third distinct maximum is 1.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
  * Find third (k th) distinct maximum in the array.
  * <p>
  * kth distinct Maximum
