c2b61800537802d54198571a42f08f0cfdc4f65f
Jesse Zhuang <zhzexi@gmail.com>
2023-02-01 19:00:25 -0800

    lc 211

--- src/main/java/tree/AddSearchWord.java
+++ src/main/java/tree/AddSearchWord.java
@@ -47,17 +47,36 @@ package tree;
 public class AddSearchWord {
 }
 
-class WordDictionary {
+class WordDictionary { // 452 ms, 96 Mb.
+    Node root;
 
-    public WordDictionary() {
-
+    public WordDictionary() { // N: number of words in trie.
+        root = new Node();
     }
 
     public void addWord(String word) {
-
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            int id = word.charAt(i) - 'a';
+            if (cur.next[id] == null) cur.next[id] = new Node();
+            cur = cur.next[id];
+        }
+        cur.isWord = true;
     }
 
-    public boolean search(String word) {
-        return true;
+    public boolean search(String word) { // space R*N. time: search hit O(L), search miss: Log_R(L) L: word length
+        return match(word, 0, root);
+    }
+
+    private boolean match(String word, int d, Node n) {
+        if (d == word.length()) return n.isWord;
+        int id = word.charAt(d) - 'a';
+        if (word.charAt(d) != '.') return n.next[id] != null && match(word, d + 1, n.next[id]);
+        else {
+            for (int i = 0; i < n.next.length; i++)
+                if (n.next[i] != null)
+                    if (match(word, d + 1, n.next[i])) return true;
+        }
+        return false;
     }
 }
