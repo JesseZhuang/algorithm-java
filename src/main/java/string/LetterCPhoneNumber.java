f4a8866d530dc8d40624832e8d20258ba1bebb94
Jesse Zhuang <zhzexi@gmail.com>
2023-02-07 14:58:57 -0800

    lc 17

--- src/main/java/string/LetterCPhoneNumber.java
+++ src/main/java/string/LetterCPhoneNumber.java
@@ -1,6 +1,9 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * LeetCode 17, medium, tags: hash table, string, backtracking.
@@ -30,7 +33,34 @@ import java.util.List;
  * digits[i] is a digit in the range ['2', '9'].
  */
 public class LetterCPhoneNumber {
+    // 2ms, 40.8Mb. O(3^n) time and space.
     public List<String> letterCombinations(String digits) {
-        return null;
+        if (digits.isEmpty()) return new ArrayList<>(); // expected [] not [""]
+        List<StringBuilder> res = new ArrayList<>();
+        res.add(new StringBuilder());
+        char[][] map = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}, {'j', 'k', 'l'}, {'m', 'n', 'o'},
+                {'p', 'q', 'r', 's'}, {'t', 'u', 'v'}, {'w', 'x', 'y', 'z'}};
+        for (int i = 0; i < digits.length(); i++) {
+            List<StringBuilder> temp = new ArrayList<>();
+            for (StringBuilder sb : res)
+                for (char c : map[digits.charAt(i) - '2'])
+                    temp.add(new StringBuilder(sb).append(c)); // must copy, cannot directly append
+            res = temp;
+        }
+        return res.stream().map(sb -> sb.toString()).collect(Collectors.toList());
+    }
+
+    // use a queue, another method is recursive
+    public List<String> letterCombinationsQ(String digits) {
+        LinkedList<String> ans = new LinkedList<String>();
+        if (digits.isEmpty()) return ans;
+        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        ans.add("");
+        while (ans.peek().length() != digits.length()) {
+            String remove = ans.remove();
+            String map = mapping[digits.charAt(remove.length()) - '0'];
+            for (char c : map.toCharArray()) ans.addLast(remove + c);
+        }
+        return ans;
     }
 }
