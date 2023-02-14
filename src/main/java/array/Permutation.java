878988aaa6f5d900b5288ca4adb90d5b47022162
Jesse Zhuang <zhzexi@gmail.com>
2023-02-13 16:48:07 -0800

    lc 46

--- src/main/java/array/Permutation.java
+++ src/main/java/array/Permutation.java
@@ -1,5 +1,6 @@
 package array;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,7 +29,33 @@ import java.util.List;
  * All the integers of nums are unique.
  */
 public class Permutation {
+
+    // 1ms, 42.2 Mb. O(N*N!) time, O(N) space.
     public List<List<Integer>> permute(int[] nums) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> numList = new ArrayList<>();
+        for (int n : nums) numList.add(n);
+        permute(numList, 0, res);
+        return res;
+    }
+
+    void permute(List<Integer> nums, int begin, List<List<Integer>> res) {
+        if (begin == nums.size()) {
+            res.add(new ArrayList<>(nums));
+            return;
+        }
+        for (int i = begin; i < nums.size(); i++) {
+            swap(nums, begin, i);
+            permute(nums, begin + 1, res);
+            swap(nums, begin, i);
+        }
+    }
+
+    private void swap(List<Integer> nums, int i, int j) {
+        if (i != j) {
+            int temp = nums.get(i);
+            nums.set(i, nums.get(j));
+            nums.set(j, temp);
+        }
     }
 }
