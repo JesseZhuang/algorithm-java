3bccfb3c4b93880691299f8a097ebbe5c1e9ed4a
Jesse Zhuang <zhzexi@gmail.com>
2023-02-15 11:45:30 -0800

    lc 42 other solutions

--- src/main/java/array/TrapRainWater.java
+++ src/main/java/array/TrapRainWater.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Stack;
+
 /**
  * LeetCode 42, hard, tags: array, two pointer, dynamic programming, stack, monotonic stack.
  * <p>
@@ -26,7 +28,7 @@ package array;
 public class TrapRainWater {
 
     // 1ms, 43Mb. two pointer. O(n) time, O(1) space.
-    public int trap(int[] height) {
+    public int trap2p(int[] height) {
         int left = 0, right = height.length - 1, leftWall = 0, rightWall = 0, res = 0;
         while (left < right) {
             if (height[left] < height[right]) {
@@ -41,4 +43,34 @@ public class TrapRainWater {
         }
         return res;
     }
+
+    // 1ms, 42.9 Mb, DP. O(n) time, O(n) space.
+    public int trapDP(int[] height) {
+        int res = 0, len = height.length;
+        int[] leftWall = new int[len], rightWall = new int[len];
+        leftWall[0] = height[0];
+        for (int i = 1; i < len; i++) leftWall[i] = Math.max(height[i], leftWall[i - 1]);
+        rightWall[len - 1] = height[len - 1];
+        for (int i = len - 2; i >= 0; i--) rightWall[i] = Math.max(height[i], rightWall[i + 1]);
+        for (int i = 1; i < len - 1; i++) res += Math.min(leftWall[i], rightWall[i]) - height[i];
+        return res;
+    }
+
+    // 13ms, 43.1 Mb. Stack,  O(n) time, O(n) space (worst case stairs-like or flat structure).
+    public int trapStack(int[] height) {
+        int res = 0, cur = 0;
+        Stack<Integer> s = new Stack<>();
+        while (cur < height.length) {
+            while (!s.isEmpty() && height[cur] > height[s.peek()]) {
+                int top = s.pop();
+                if (s.isEmpty()) break;
+                int distance = cur - s.peek() - 1; // rightWall - leftWall - 1
+                int h = Math.min(height[cur], height[s.peek()]) - height[top];
+                res += distance * h;
+            }
+            s.push(cur++);
+        }
+        return res;
+    }
+
 }
