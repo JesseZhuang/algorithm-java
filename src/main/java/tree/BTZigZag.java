608c4163674b8a2564e121a9728fa03064f1f4ee
Jesse Zhuang <zhzexi@gmail.com>
2023-03-31 17:00:56 -0700

    lc 103 q

---
+++ src/main/java/tree/BTZigZag.java
@@ -0,0 +1,36 @@
+package tree;
+
+import struct.TreeNode;
+
+import java.util.List;
+
+/**
+ * LeetCode 103, medium, tags: tree, bfs, binary tree.
+ * <p>
+ * Given the root of a binary tree, return the zigzag level order traversal of its nodes' values.
+ * (i.e., from left to right, then right to left for the next level and alternate between).
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: [[3],[20,9],[15,7]]
+ * Example 2:
+ * <p>
+ * Input: root = [1]
+ * Output: [[1]]
+ * Example 3:
+ * <p>
+ * Input: root = []
+ * Output: []
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the tree is in the range [0, 2000].
+ * -100 <= Node.val <= 100
+ */
+public class BTZigZag {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        return null;
+    }
+
+}
