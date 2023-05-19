716d557d7065c26e2dfb1fc88e8677ebff20ac8d
Jesse Zhuang <zhzexi@gmail.com>
2023-05-19 16:55:14 -0700

    lc 108 q

---
+++ src/main/java/tree/SortedArrayBST.java
@@ -0,0 +1,29 @@
+package tree;
+
+/**
+ * LeetCode 108, easy, tags: array, tree, binary search tree, binary tree.
+ * <p>
+ * Given an integer array nums where the elements are sorted in ascending order,
+ * convert it to a height-balanced binary search tree.
+ * <p>
+ * Example 1: https://assets.leetcode.com/uploads/2021/02/18/btree1.jpg
+ * <p>
+ * Input: nums = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+ * https://assets.leetcode.com/uploads/2021/02/18/btree2.jpg
+ * <p>
+ * Example 2: https://assets.leetcode.com/uploads/2021/02/18/btree.jpg
+ * <p>
+ * Input: nums = [1,3]
+ * Output: [3,1]
+ * Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums is sorted in a strictly increasing order.
+ */
+public class SortedArrayBST {
+}
