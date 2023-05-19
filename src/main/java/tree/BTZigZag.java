36f0ca0a5cd3ec5521c4b279f73b7cc178bf9ef6
Jesse Zhuang <zhzexi@gmail.com>
2023-05-19 16:32:16 -0700

    lc 103, dfs

--- src/main/java/tree/BTZigZag.java
+++ src/main/java/tree/BTZigZag.java
@@ -2,6 +2,8 @@ package tree;
 
 import struct.TreeNode;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,8 +31,21 @@ import java.util.List;
  * -100 <= Node.val <= 100
  */
 public class BTZigZag {
+    // 0ms, 41.5Mb. O(N) time and space (result and recursive stack).
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(root, res, 0);
+        return res;
+    }
+
+    private void dfs(TreeNode cur, List<List<Integer>> res, int level) {
+        if (cur == null) return;
+        if (res.size() <= level) res.add(new LinkedList<>()); // important: linked list
+        List<Integer> curLevel = res.get(level);
+        if (level % 2 == 0) curLevel.add(cur.val);
+        else curLevel.add(0, cur.val);
+        dfs(cur.left, res, level + 1);
+        dfs(cur.right, res, level + 1);
     }
 
 }
