723a052cf95daa1694802f7280cc08b2d0d15a69
Jesse Zhuang <zhzexi@gmail.com>
2023-06-05 15:12:24 -0700

    lc 131 q

---
+++ src/main/java/dp/PalindromePartitioning.java
@@ -0,0 +1,24 @@
+package dp;
+
+/**
+ * LeetCode 131, medium, tags: string, dynamic programming, backtracking.
+ * <p>
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+ * Return all possible palindrome partitioning of s.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "aab"
+ * Output: [["a","a","b"],["aa","b"]]
+ * Example 2:
+ * <p>
+ * Input: s = "a"
+ * Output: [["a"]]
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 16
+ * s contains only lowercase English letters.
+ */
+public class PalindromePartitioning {
+}
