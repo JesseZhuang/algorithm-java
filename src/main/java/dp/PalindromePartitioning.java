92baae8d6bf2a6452b7461b4284c27761111d5aa
Jesse Zhuang <zhzexi@gmail.com>
2023-06-09 14:50:12 -0700

    lc 131 dp

--- src/main/java/dp/PalindromePartitioning.java
+++ src/main/java/dp/PalindromePartitioning.java
@@ -25,6 +25,28 @@ import java.util.List;
  */
 public class PalindromePartitioning {
 
+    // 20ms, 55.5 Mb. O(N*2^N) time, O(N) space
+    public List<List<String>> partition(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList<>();
+        dfs(result, s, 0, new ArrayList<>(), dp);
+        return result;
+    }
+
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+        if (start >= s.length()) result.add(new ArrayList<>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+
+
     // 10ms, 55.3 Mb. O(N*2^N) time, O(N) space (recursion stack)
     public List<List<String>> partitionDFS(String s) {
         List<List<String>> result = new ArrayList<>();
