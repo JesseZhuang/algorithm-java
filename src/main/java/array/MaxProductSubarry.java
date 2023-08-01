e7b05f7398809716582da4faa75965d2c16778f2
Jesse Zhuang <zhzexi@gmail.com>
2023-07-31 22:18:37 -0700

    lc 152 refactor

--- src/main/java/array/MaxProductSubarry.java
+++ src/main/java/array/MaxProductSubarry.java
@@ -24,7 +24,7 @@ package array;
  * <p>
  * Constraints:
  * <pre>
- * 1 <= nums.length <= 2 * 104
+ * 1 <= nums.length <= 2 * 10^4
  * -10 <= nums[i] <= 10
  * </pre>
  * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
@@ -33,13 +33,15 @@ public class MaxProductSubarry {
     /**
      * Kadane's algorithm. DP. O(N) time, O(1) space. 7ms, 45.3 Mb.
      */
-    public int maxProduct(int[] nums) {
+    public int maxProduct(int[] nums) { // [2,3,-2,4]
         int maxEndingHere = 1, minEndingHere = 1, maxSoFar = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
             int prod1 = maxEndingHere * nums[i], prod2 = minEndingHere * nums[i];
-            maxEndingHere = Math.max(nums[i],  Math.max(prod1, prod2));
-            minEndingHere = Math.min(nums[i],  Math.min(prod1, prod2));
-            maxSoFar = Math.max(maxEndingHere, maxSoFar);
+            maxEndingHere = Math.max(nums[i], Math.max(prod1, prod2));
+            // (2,(2,2)) -> 2, (3,(6,6)) -> 6, (-2,(-12,-6)) -> -2, (4, (-8,-48)) -> 4
+            minEndingHere = Math.min(nums[i], Math.min(prod1, prod2));
+            // (2,(2,2)) -> 2, (3,(6,6)) -> 3, (-2, (-12,-6)) -> -12, (4, (-8,-48)) -> -48
+            maxSoFar = Math.max(maxEndingHere, maxSoFar); // 2,6,6,6
         }
         return maxSoFar;
     }
