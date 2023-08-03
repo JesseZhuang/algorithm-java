d6eb39cbbba0a9824c3183b6202f98f1b661fda9
Jesse Zhuang <zhzexi@gmail.com>
2023-08-03 16:02:19 -0700

    lc 148 question

---
+++ src/main/java/list/SortList.java
@@ -0,0 +1,34 @@
+package list;
+
+import struct.ListNode;
+
+/**
+ * LeetCode 148, medium, tags: linked list, two pointers, divide and conquer, sorting, merge sort.
+ * <p>
+ * Given the head of a linked list, return the list after sorting it in ascending order.
+ * <p>
+ * Example 1: https://assets.leetcode.com/uploads/2020/09/14/sort_list_1.jpg
+ * <p>
+ * Input: head = [4,2,1,3]
+ * Output: [1,2,3,4]
+ * Example 2: https://assets.leetcode.com/uploads/2020/09/14/sort_list_2.jpg
+ * <p>
+ * Input: head = [-1,5,3,4,0]
+ * Output: [-1,0,3,4,5]
+ * Example 3:
+ * <p>
+ * Input: head = []
+ * Output: []
+ * <p>
+ * Constraints:
+ * <p>
+ * The number of nodes in the list is in the range [0, 5 * 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * <p>
+ * Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+ */
+public class SortList {
+    public ListNode sortList(ListNode head) {
+        return null;
+    }
+}
