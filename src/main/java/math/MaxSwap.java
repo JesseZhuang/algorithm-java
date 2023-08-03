0ce90d6903bcf5f931acd10d129c5f572a27c012
Jesse Zhuang <zhzexi@gmail.com>
2023-08-02 18:53:15 -0700

    lc 670 two solutions

--- src/main/java/math/MaxSwap.java
+++ src/main/java/math/MaxSwap.java
@@ -20,10 +20,53 @@ package math;
  * <p>
  * Constraints:
  * <p>
- * 0 <= num <= 10^8
+ * 0 <= num <= 10^8, N digits
  */
 public class MaxSwap {
+    // 0ms, 39.3 Mb, one pass
     public int maximumSwap(int num) {
-        return 0;
+        char[] digits = Integer.toString(num).toCharArray();
+        int maxIndex = digits.length - 1, leftIndex = -1, rightIndex = -1;
+        for (int i = digits.length - 2; i >= 0; i--) {
+            if (digits[i] > digits[maxIndex]) maxIndex = i;
+            else if (digits[i] < digits[maxIndex]) { // best candidate for max swap
+                leftIndex = i;
+                rightIndex = maxIndex;
+            }
+        }
+        if (leftIndex == -1) return num;
+        char tmp = digits[rightIndex];
+        digits[rightIndex] = digits[leftIndex];
+        digits[leftIndex] = tmp;
+        return Integer.valueOf(new String(digits));
+    }
+
+    // 0ms, 39.3Mb, bucket to remember index (rightmost), O(N) time, O(1) space.
+    public int maximumSwapBucket(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+        int[] buckets = new int[10]; // digit -> index
+        for (int i = 0; i < digits.length; i++) buckets[digits[i] - '0'] = i;
+        int max = 9;
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = max; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+            max = digits[i] - '0';
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        System.out.println('0' - 1); // 47
+        System.out.println(Integer.valueOf('0')); // 48, char '0'
+        System.out.println('0' > ('0' - 1));
+        char maxDigit = '0' - 1;
+        System.out.println(maxDigit); // '/'
+        System.out.println(Character.toChars(47)); // '/'
     }
 }
