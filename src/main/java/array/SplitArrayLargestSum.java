6b228dd3e1e0c60bf691b6e38b2877efd7c6b960
Jesse Zhuang <zhzexi@gmail.com>
2023-08-03 17:35:42 -0700

    lc 410

--- src/main/java/array/SplitArrayLargestSum.java
+++ src/main/java/array/SplitArrayLargestSum.java
@@ -25,12 +25,42 @@ package array;
  * <p>
  * Constraints:
  * <p>
- * 1 <= nums.length <= 1000
- * 0 <= nums[i] <= 106
+ * 1 <= nums.length <= 1000, n
+ * 0 <= nums[i] <= 10^6
  * 1 <= k <= min(50, nums.length)
  */
 public class SplitArrayLargestSum {
+
+    // O(lg(sum)*n) time, O(1) space. 0ms, 40.1 Mb.
     public int splitArray(int[] nums, int k) {
-        return 0;
+        int max = 0;
+        long sum = 0;
+        for (int num : nums) {
+            max = Math.max(num, max);
+            sum += num;
+        }
+        if (k == 1) return (int) sum;
+        long l = max;
+        long r = sum;
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            if (valid(mid, nums, k)) r = mid - 1;
+            else l = mid + 1;
+        }
+        return (int) l;
+    }
+
+    public boolean valid(long target, int[] nums, int k) {
+        int count = 1;
+        long total = 0;
+        for (int num : nums) {
+            total += num;
+            if (total > target) {
+                total = num;
+                count++;
+                if (count > k) return false;
+            }
+        }
+        return true;
     }
 }
