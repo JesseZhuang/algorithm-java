7e5f19cc9338d5eac403532c3da803fb9d7a80bf
Jesse Zhuang <zhzexi@gmail.com>
2023-08-21 16:57:39 -0700

    lc question 149

---
+++ src/main/java/array/MaxPointsLine.java
@@ -0,0 +1,29 @@
+package array;
+
+/**
+ * LeetCode 149, hard, tags: array, hash table, math, geometry.
+ * <p>
+ * Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum
+ * number of points that lie on the same straight line.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: points = [[1,1],[2,2],[3,3]]
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+ * Output: 4
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= points.length <= 300
+ * points[i].length == 2
+ * -10^4 <= xi, yi <= 10^4
+ * All the points are unique.
+ */
+public class MaxPointsLine {
+    public int maxPoints(int[][] points) {
+        return 0;
+    }
+}
