b55b71b890cf68f743bff3b6ceeaa1a7731ff50f
Jesse Zhuang <zhzexi@gmail.com>
2023-11-07 13:17:02 -0800

    lc 121 practice

--- src/main/java/dp/BuySellStock.java
+++ src/main/java/dp/BuySellStock.java
@@ -22,7 +22,7 @@ package dp;
  * <p>
  * <ul>
  * <li>use peak array, 2ms 46%, O(n) time, O(n) space.
- * <li>one scan maintain two variables, O(n) time, O(1) space.
+ * <li>one scan maintain two variables (minSoFar, res), O(n) time, O(1) space.
  * <li>Kadane's algorithm, wikipedia maximum sub-array problem, O(n) time, O(1) space.
  * </ul>
  * <p>
@@ -32,57 +32,26 @@ package dp;
  * 0 <= prices[i] <= 10^4
  */
 public class BuySellStock {
-    /**
-     * 2ms 46%. O(n) time and space.
-     */
-    public int maxProfitPeakArray(int[] prices) {
-        // peak (max) for prices array in range [i, end]
-        int[] maxSoFar = new int[prices.length];
-        int max = Integer.MIN_VALUE;
-        for (int i = prices.length - 1; i > 0; i--)
-            max = maxSoFar[i] = Math.max(max, prices[i]);
-        int profit = 0;
-        for (int i = 0; i < prices.length - 1; i++)
-            profit = Math.max(profit, maxSoFar[i + 1] - prices[i]);
-        // max profit if purchased on day i
-        return profit;
-    }
-
     // 2 ms 60.9 MB. O(n) time and O(1) space.
     public int maxProfitMinPrice(int[] prices) {
         int maxPro = 0;
-        int minPrice = Integer.MAX_VALUE;
+        int minSoFar = Integer.MAX_VALUE;
         for (int price : prices) {
-            minPrice = Math.min(minPrice, price);
+            minSoFar = Math.min(minSoFar, price);
             // max profit if sold at current index
-            maxPro = Math.max(maxPro, price - minPrice);
+            maxPro = Math.max(maxPro, price - minSoFar);
         }
         return maxPro;
     }
 
     // 3ms, 61 Mb. O(n) time O(1) space.
     public int maxProfitKadane(int[] prices) { // 7 1 5 3 6 4
-        int maxSellHere = 0, maxSoFar = 0;
+        int maxSellHere = 0, res = 0;
         for (int i = 1; i < prices.length; i++) {
             // max profit if sold at current index, important first item 0
             maxSellHere = Math.max(0, maxSellHere + (prices[i] - prices[i - 1])); // 0 4 2 5 3
-            maxSoFar = Math.max(maxSellHere, maxSoFar); // 0 4 4 5 5
+            res = Math.max(maxSellHere, res); // 0 4 4 5 5
         }
-        return maxSoFar; // 5
-    }
-
-    @Deprecated
-    public int maxProfit2Pointer(int[] prices) {
-        int profit = 0;
-        int i = 0, j = prices.length - 1;
-        while (i < j) {
-            while (i < j - 1 && prices[i + 1] < prices[i])
-                i++;
-            while (i < j - 1 && prices[j - 1] > prices[j])
-                j--;
-            // flaw for { 3, 2, 6, 5, 0, 3 }
-            profit = Math.max(profit, prices[j] - prices[i++]);
-        }
-        return profit;
+        return res; // 5
     }
 }
