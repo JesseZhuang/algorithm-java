ab1e816a0322a790cd3720d7982d4039f3f99158
Jesse Zhuang <zhzexi@gmail.com>
2023-11-09 20:42:40 -0800

    practice lc 283

--- src/main/java/array/MoveZeroes.java
+++ src/main/java/array/MoveZeroes.java
@@ -24,20 +24,22 @@ package array;
  * <p>
  * 1 <= nums.length <= 10^4
  * -2^31 <= nums[i] <= 2^31 - 1
- * <b>Summary:</b>
  * <p>
- * <ul>
- * <li><b>Iterative, swapping/setting, O(n) time, O(1) space.</b>
- * <li>Recursive, O(n) time, O(1) space.
- * </ul>
+ * Follow up: Could you minimize the total number of operations done?
+ * <p>
+ * Hints:
+ * In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing
+ * array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem
+ * as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
+ * <p>
+ * A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and
+ * another pointer that just works on the non-zero elements of the array.
  */
 public class MoveZeroes {
 
     // solution 1, O(n) time, O(1) space. 2ms, 44.62Mb.
     public void moveZeroesIter2(int[] nums) {
-        if (nums == null) return;
-        int zeroInd = 0; // The index of the leftmost zero in nums.
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0, zeroInd = 0; i < nums.length; i++) { // The index of the leftmost zero in nums.
             if (nums[i] != 0) {
                 if (i > zeroInd) { // i can only be larger than or equal to zeroInd. {2, 1, 3, 0, 5, 0, 6}
                     nums[zeroInd] = nums[i];
@@ -50,9 +52,9 @@ public class MoveZeroes {
 
     // solution 2, O(n+k) time, O(1) space. k == number of zeroes. 1ms, 44.98 Mb.
     public void moveZeroesIter1(int[] nums) {
-        if (nums == null) return;
-        int insert = 0;
-        for (int n : nums) if (n != 0 && nums[insert++] != n) nums[insert - 1] = n;
+        int insert = 0, l = nums.length;
+        for (int i = 0; i < l; i++)
+            if (nums[i] != 0 && insert++ != i) nums[insert - 1] = nums[i];
         while (insert < nums.length) nums[insert++] = 0;
     }
 
