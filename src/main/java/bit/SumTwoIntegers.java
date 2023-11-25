3c88a135f8c94e53f3bce472e8b867bd1c1b07f6
Jesse Zhuang <zhzexi@gmail.com>
2023-11-25 13:02:54 -0800

    lc 317 practice

--- src/main/java/bit/SumTwoIntegers.java
+++ src/main/java/bit/SumTwoIntegers.java
@@ -17,17 +17,18 @@ package bit;
  * <li><b>O(carry) time, O(1) space, bit iterative or recursive, 0ms, 7%.</b>
  * <li>O(32) time, O(1) space, bit 32 one by one radix, 0 ms, 7%.
  * </ul>
- *
- * @author Zexi Jesse Zhuang
+ * Constraints:
+ * <p>
+ * -1000 <= a, b <= 1000
  */
 public class SumTwoIntegers {
 
+    // solution 1, iterative, 0ms, 39.35Mb. O(carry) time, O(1) space.
     public int getSumIterative(int a, int b) {
-        if (a == 0) return b;
-        if (b == 0) return a;
+        if (a == 0 || b == 0) return a | b;
         while (b != 0) {
             int carry = a & b;
-            a = a ^ b; // bit XOR same effect as bit wise adding
+            a ^= b; // store sum without carry in a
             b = carry << 1;
         }
         return a;
