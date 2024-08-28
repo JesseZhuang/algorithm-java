4e6dbbed6b4851664eda3dc6bfe61d90b7fbb466
Jesse Zhuang <zhzexi@gmail.com>
2024-08-27 17:34:40 -0700

    lc 600 q

---
+++ src/main/java/dp/NNIWOCOnes.java
@@ -0,0 +1,37 @@
+package dp;
+
+/**
+ * LeetCode 600, hard, tags: dynamic programming.
+ * <p>
+ * Given a positive integer n, return the number of the integers in the range [0, n]
+ * whose binary representations do not contain consecutive ones.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 5
+ * Output: 5
+ * Explanation:
+ * Here are the non-negative integers <= 5 with their corresponding binary representations:
+ * 0 : 0
+ * 1 : 1
+ * 2 : 10
+ * 3 : 11
+ * 4 : 100
+ * 5 : 101
+ * Among them, only integer 3 disobeys the rule (two consecutive ones) and the other 5 satisfy the rule.
+ * Example 2:
+ * <p>
+ * Input: n = 1
+ * Output: 2
+ * Example 3:
+ * <p>
+ * Input: n = 2
+ * Output: 3
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 10^9
+ */
+public class NNIWOCOnes {
+}
