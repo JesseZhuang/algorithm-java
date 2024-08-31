f47761ab04aaabbb8288bb1586124b7057ee5867
Jesse Zhuang <zhzexi@gmail.com>
2024-08-30 18:04:32 -0700

    lc practice

--- src/main/java/array/WordSearch.java
+++ src/main/java/array/WordSearch.java
@@ -31,7 +31,7 @@ package array;
  */
 public class WordSearch {
     // 383ms, 42.5Mb. O(mn 3^l) time, O(mn) space, O(min(l,mn)) recursion space.
-    // Some use bitwise XOR 256 or set char to not letter to save space
+    // Some use bitwise XOR 256 or set char to not letter to save space (visited array)
     public boolean exist(char[][] board, String word) {
         boolean[][] visited = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++)
@@ -42,7 +42,7 @@ public class WordSearch {
 
     private boolean dfs(char[][] board, String word, int index, boolean[][] visited, int i, int j) {
         if (index == word.length()) return true;
-        if (i >= board.length || i < 0 || j < 0 || j >= board[0].length
+        if (i >= board.length || j >= board[0].length || i < 0 || j < 0
                 || visited[i][j] || board[i][j] != word.charAt(index)) return false; // board[i][j]=='8' visited
         visited[i][j] = true; // board[i][j] ^= 256;
         int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
