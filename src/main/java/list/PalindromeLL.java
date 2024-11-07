331cded643ada0a1555491e1c9d9dea639c4a9ab
Jesse Zhuang <zhzexi@gmail.com>
2024-11-06 22:23:40 -0800

    lc practice

--- src/main/java/list/PalindromeLL.java
+++ src/main/java/list/PalindromeLL.java
@@ -5,9 +5,7 @@ import struct.ListNode;
 /**
  * LeetCode 234, LintCode 223, easy, tags: linked list, two pointers, stack, recursion.
  * <p>
- * Given the head of a singly linked list, return true if it is a
- * palindrome
- * or false otherwise.
+ * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
  * <p>
  * Example 1:
  * <p>
@@ -23,7 +21,7 @@ import struct.ListNode;
  * <p>
  * Constraints:
  * <p>
- * The number of nodes in the list is in the range [1, 105].
+ * The number of nodes in the list is in the range [1, 10^5].
  * 0 <= Node.val <= 9
  * <p>
  * <p>
@@ -31,54 +29,27 @@ import struct.ListNode;
  */
 @SuppressWarnings("unused")
 public class PalindromeLL {
-    // todo
+    // n,1.
     static class Solution {
-        public boolean isPalindrome(ListNode head) {
-            if (head == null) {
-                return true;
-            }
-
-            // 找到前半部分链表的尾节点并反转后半部分链表
-            ListNode firstHalfEnd = endOfFirstHalf(head);
-            ListNode secondHalfStart = reverseList(firstHalfEnd.next);
-
-            // 判断是否回文
-            ListNode p1 = head;
-            ListNode p2 = secondHalfStart;
-            boolean result = true;
-            while (result && p2 != null) {
-                if (p1.val != p2.val) {
-                    result = false;
-                }
-                p1 = p1.next;
-                p2 = p2.next;
-            }
-
-            // 还原链表并返回结果
-            firstHalfEnd.next = reverseList(secondHalfStart);
-            return result;
-        }
-
-        private ListNode reverseList(ListNode head) {
-            ListNode prev = null;
-            ListNode curr = head;
-            while (curr != null) {
-                ListNode nextTemp = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = nextTemp;
-            }
-            return prev;
-        }
-
-        private ListNode endOfFirstHalf(ListNode head) {
-            ListNode fast = head;
-            ListNode slow = head;
-            while (fast.next != null && fast.next.next != null) {
+        public boolean isPalindrome(ListNode head) { // 1->2->1->#
+            ListNode rev = null, slow = head, fast = head;
+            while (fast != null && fast.next != null) {
                 fast = fast.next.next;
+                ListNode temp = rev;
+                rev = slow; // reverse first half
                 slow = slow.next;
-            }
-            return slow;
+                rev.next = temp;
+            } // #<-1:rev slow:2->1->#, fast:1->#
+            ListNode tail = fast == null ? slow : slow.next; // tail:1->#, odd number of nodes
+            while (rev != null) {
+                if (tail.val != rev.val) return false;
+                tail = tail.next;
+                ListNode temp = slow; // play nice, reverse first half again
+                slow = rev;
+                rev = rev.next;
+                slow.next = temp;
+            } // 1->2->1->#
+            return true;
         }
     }
 }
