8d32303b6c8427a187d29a1cca4523746d89cdee
Jesse Zhuang <zhzexi@gmail.com>
2024-11-10 19:45:14 -0800

    lc practice

--- src/test/java/bit/UpdateBitsTest.java
+++ src/test/java/bit/UpdateBitsTest.java
@@ -1,6 +1,7 @@
 package bit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,12 +18,20 @@ class UpdateBitsTest {
 
     @ParameterizedTest
     @CsvSource({
+            "-521,0,31,31,2147483127",
             "1024,21,2,6,1108",
             "1024,31,2,6,1148",
             "1024,31,2,31,124",
     })
     void test(int N, int M, int i, int j, int exp) {
-        System.out.println();
         assertEquals(exp, tbt.updateBits(N, M, i, j));
+    }
+
+    @Test
+    void test_overflow() {
+        assertEquals(-1, 0xffff_ffff);
+        assertEquals(1 << 31, Integer.MIN_VALUE);
+        assertEquals(1 << 31 - 1, 1 << 30); // precedence
+        assertEquals((1 << 31) - 1, Integer.MAX_VALUE);
     }
 }
